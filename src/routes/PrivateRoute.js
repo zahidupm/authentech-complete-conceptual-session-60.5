a087b2659f94b99360dc5d4ac95d1dc5664b0b9f
zahidupm <zahidhasanlimon111@gmail.com>
2022-10-22 19:50:11 +0600

    loading center

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -7,7 +7,11 @@ const PrivateRoute = ({children}) => {
     const location = useLocation();
 
     if(loading) {
-        return <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
+        return (
+            <div className='flex items-center justify-center min-h-screen'>
+                <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
+            </div>
+        )
     }
 
     if(user && user.uid) {
