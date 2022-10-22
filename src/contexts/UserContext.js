c847a86c5450f04b9f447a40200466fb993098eb
zahidupm <zahidhasanlimon111@gmail.com>
2022-10-22 19:39:47 +0600

    how to set loading and setLoadingstate

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -8,14 +8,17 @@ export const AuthContext = createContext();
 const UserContext = ({children}) => {
     const googleProvider = new GoogleAuthProvider();
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     // 1. create user
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
     // 2. update name
     const updateName = (name) => {
+        setLoading(true);
         return updateProfile(auth.currentUser, {
             displayName: name
         })
@@ -23,26 +26,31 @@ const UserContext = ({children}) => {
 
     // 3. email verify
     const verifyEmail = () => {
+        setLoading(true);
         return sendEmailVerification(auth.currentUser);
     }
 
     // 4. google signIn
     const signInWithGoogle = () => {
+        setLoading(true);
         return signInWithPopup(auth, googleProvider);
     }
 
     // 5. logout
     const logOut = () => {
+        setLoading(true);
         return signOut(auth);
     }
 
     // 6. login with password
     const signIn = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
     // 7. forget password
     const resetPassword = (email) => {
+        setLoading(true);
         return sendPasswordResetEmail(auth, email);
     }
 
@@ -54,13 +62,15 @@ const UserContext = ({children}) => {
         signInWithGoogle,
         logOut,
         signIn,
-        resetPassword
+        resetPassword,
+        loading
     };
 
     useEffect(() => {
         // eta run hobe jokhon component  mount hobe
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
+            setLoading(false);
         })
         return () => {
           // eta run hobe johkon component unmount hobe
--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -3,8 +3,12 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../contexts/UserContext';
 
 const PrivateRoute = ({children}) => {
-    const {user} = useContext(AuthContext);
+    const {user, loading} = useContext(AuthContext);
     const location = useLocation();
+
+    if(loading) {
+        return <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
+    }
 
     if(user && user.uid) {
         return children;
